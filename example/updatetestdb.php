e457e6fa74d99e39b4820556492d90f0fffb3406
hhdectu <hhde@ctu.edu.vn>
2020-08-25 19:02:52 +0700

    update su dung <<< trong chuoi

---
+++ example/updatetestdb.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<h1> update php</h1>
+
+<?php
+include_once(__DIR__ .'/../dbconnect.php');
+
+$chuyenkhoan='Chuyển khoản bằng ví momo';
+$sql=<<<EOT
+    update  `hinhthucthanhtoan` set httt_ten='$chuyenkhoan' where httt_ma=2
+
+EOT;
+
+
+
+mysqli_query($conn, $sql);
+
+?>
+
+    
+</body>
+</html>
